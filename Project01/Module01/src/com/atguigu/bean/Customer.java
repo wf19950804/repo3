ef3d0b2626b132a71f24114edeebb3bb58f24f06
wf19950804 <727684204@qq.com>
2020-02-02 16:45:23 +0800

    将java工程添加到版本库

---
+++ Project01/Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+//这是单行注释
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("helloworld!!!!!!");
+
+        ArrayList list = new ArrayList();
+//        data data = new data();
+         method1();
+
+    }
+    public static void method1(){
+
+    }
+
+}
---
+++ Project01/src/com/atguigu/java/Hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2019-08-19 17:41
+ */
+public class Hello {
+}
